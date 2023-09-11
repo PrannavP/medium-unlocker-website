f3640ad46daca70b307ec7922a2f40b11820f7ac
PrannavP <ramsarilekhi@gmail.com>
2023-09-11 16:44:13 +0545

    Removed Test

--- test/regex.js
+++
@@ -1,8 +0,0 @@
-const regex = /medium\.com/;
-const myString = "https://prannavpanta.medium.com/my-first-blog/12423";
-
-if(!regex.test(myString)){
-    alert("Invalid URL!!! i guess... hehe");
-}else{
-    window.open(toRedirectURL, '_blank');
-};
